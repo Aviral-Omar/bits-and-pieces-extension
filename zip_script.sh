23dbac723456021f56e88f8a19838c83c10401f1
Aviral-Omar <aviralomar0301@gmail.com>
2022-03-04 19:39:15 +0530

    fixed script

--- zip_script.sh
+++ zip_script.sh
@@ -2,15 +2,15 @@
 cd build/
 
 cp ../firefox/manifest.json manifest.json
-zip -r ../firefox/bits-compendium.zip *
+zip -FSr ../firefox/bits-compendium.zip *
 
 cp ../chrome/manifest.json manifest.json
-zip -r ../chrome/bits-compendium.zip *
+zip -FSr ../chrome/bits-compendium.zip *
 
 cp ../edge/manifest.json manifest.json
-zip -r ../edge/bits-compendium.zip *
+zip -FSr ../edge/bits-compendium.zip *
 
 cp ../firefox/manifest.json manifest.json
 
 cd ..
-zip -r firefox/bits-compendium-source.zip package.json package-lock.json public/ src/ README.md
+zip -FSr firefox/bits-compendium-source.zip package.json package-lock.json public/ src/ README.md
